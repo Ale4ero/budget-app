a2eca1ac7895003df20403ad357537d79c1bdc1d
Ale4ero <ale.forero10@gmail.com>
2023-02-02 14:40:03 -0500

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <Container className="my-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
+        <Button variant="primary">Add Category</Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack> 
       <div style={{ 
@@ -16,7 +16,7 @@ function App() {
         gap: "1rem",
         alignItems: "flex-start",
       }}>
-        <BudgetCard gray name="Food" amount={200} max={1000}></BudgetCard>
+        <BudgetCard gray name="Food" amount={2000} max={1000}></BudgetCard>
       </div>
     </Container>
   )
