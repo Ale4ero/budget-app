11c37e238d4ebc1c79006dd195de635253211872
Ale4ero <ale.forero10@gmail.com>
2023-02-16 14:56:11 -0500

    Root Prototype pt1
    Add Expense feature
    View Expense feature
    Uncategorized Category
    Total Budget Card

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import { useState } from "react";
 import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import AddBudgetModal from "./components/AddBudgetModal";
+import AddExpenseModal from "./components/AddExpenseModal";
+import ViewExpensesModal from "./components/ViewExpensesModal";
 import BudgetCard from "./components/BudgetCard";
-import { useBudgets } from "./contexts/BudgetContext";
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard"
+import TotalBudgetCard from "./components/TotalBudgetCard"
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetContext";
 
 function App() {
   //useState for addCategory button
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+
+  //useState for addExpense button
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
+
+  //useState for viewExpenses button FOR SPECIFIC BUDGET
+  const [viewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState()
+
+  //useState for addExpense button FOR SPECIFIC BUDGET
+  const [addExpenseModalBudgetId, setAddExpenseModalModalId] = useState()
+
   //getting budgets from useBudgets context
   const {budgets, getBudgetExpenses} = useBudgets()
+
+  function openAddExpenseModal(budgetId){
+    setShowAddExpenseModal(true)
+    setAddExpenseModalModalId(budgetId)
+  }
 
   return (
     <>
@@ -17,31 +36,49 @@ function App() {
         <Stack direction="horizontal" gap="2" className="mb-4">
           <h1 className="me-auto">Budgets</h1>
           <Button variant="primary" onClick={()=> setShowAddBudgetModal(true)}>Add Category</Button>
-          <Button variant="outline-primary">Add Expense</Button>
+          <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
         </Stack> 
         <div style={{ 
-          display: "grid", 
-          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          gap: "1rem",
-          alignItems: "flex-start",
+            display: "grid", 
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
         }}>
           {budgets.map(budget =>{
             const amount = getBudgetExpenses(budget.id).reduce(
-              (total, expense) => total + expense.amount, 0
-            )
+              (total, expense) => total + expense.amount, 0)
             return (
-            <BudgetCard 
-              key={budget.id}
-              name={budget.name} 
-              amount={amount} 
-              max={budget.max}
-            />)
-            
+              <BudgetCard 
+                key={budget.id}
+                name={budget.name} 
+                amount={amount} 
+                max={budget.max}
+                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+                onViewExpensesClick={() => setViewExpensesModalBudgetId(budget.id)}
+              />
+            )   
         })}
-          
+        <UncategorizedBudgetCard 
+        onAddExpenseClick= {openAddExpenseModal}
+        onViewExpensesClick={() => setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)}/>
+        <TotalBudgetCard/>
         </div>
+
+
       </Container>
-      <AddBudgetModal show={showAddBudgetModal} handleClose={()=> setShowAddBudgetModal(false)} />
+      <AddBudgetModal 
+        show={showAddBudgetModal} 
+        handleClose={()=> setShowAddBudgetModal(false)} 
+      />
+      <AddExpenseModal 
+        show={showAddExpenseModal} 
+        defaultBudgetId={addExpenseModalBudgetId}
+        handleClose={()=> setShowAddExpenseModal(false)} 
+      />
+      <ViewExpensesModal 
+        budgetId={viewExpensesModalBudgetId}
+        handleClose={()=> setViewExpensesModalBudgetId()} 
+      />
     </>
   )
 }
--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,7 +1,7 @@
 import { Card, ProgressBar, Stack, Button } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
 
-export default function BudgetCard({name, amount, max, gray}) {
+export default function BudgetCard({name, amount, max, gray, hideButtons, onAddExpenseClick, onViewExpensesClick}) {
 
     const classNames = []
 
@@ -18,19 +18,28 @@ export default function BudgetCard({name, amount, max, gray}) {
                     <div className="me-2">{name}</div>
                     <div className="d-flex align-items-baseline">
                         {currencyFormatter.format(amount)} 
-                        <span className="text-muted fs-6 ms-1">/ {currencyFormatter.format(max)}</span>
+                        {/* only disply max if there is a max, uncategorized doesnt have a max */}
+                        {max && (
+                            <span className="text-muted fs-6 ms-1">/ {currencyFormatter.format(max)}</span>
+                        )}
                     </div>
                 </Card.Title>
-                <ProgressBar className="rounded-pill"
-                variant={getProgressBarVariant(amount, max)}
-                min={0}
-                max={max}
-                now={amount}
-                ></ProgressBar>
-                <Stack direction="horizontal" gap={2} className="mt-4">
-                    <Button variant="outline-primary" className="ms-auto">Add Expense</Button>
-                    <Button variant="outline-secondary">View Expense</Button>
-                </Stack>
+                {max&&(
+                    <ProgressBar className="rounded-pill"
+                    variant={getProgressBarVariant(amount, max)}
+                    min={0}
+                    max={max}
+                    now={amount}
+                    ></ProgressBar>
+                )}
+                
+                {!hideButtons &&(
+                    <Stack direction="horizontal" gap={2} className="mt-4">
+                    <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>Add Expense</Button>
+                    <Button variant="outline-secondary" onClick={onViewExpensesClick}>View Expense</Button>
+                    </Stack>
+                )}
+                
             </Card.Body>
         </Card>
     )
