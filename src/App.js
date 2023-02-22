64ea11bed36c90fe3d9f16f48c25c790bf909372
Ale4ero <ale.forero10@gmail.com>
2023-02-22 18:20:09 -0500

    Root Prototype Complete
    Full add category functionality.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import BudgetCard from "./components/BudgetCard";
 import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard"
 import TotalBudgetCard from "./components/TotalBudgetCard"
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetContext";
+import Sidebar from "./components/Sidebar";
+import './App.css'
+
+
 
 function App() {
   //useState for addCategory button
@@ -32,40 +36,60 @@ function App() {
 
   return (
     <>
-      <Container className="my-4">
-        <Stack direction="horizontal" gap="2" className="mb-4">
-          <h1 className="me-auto">Budgets</h1>
-          <Button variant="primary" onClick={()=> setShowAddBudgetModal(true)}>Add Category</Button>
-          <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
-        </Stack> 
-        <div style={{ 
-            display: "grid", 
-            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-            gap: "1rem",
-            alignItems: "flex-start",
-        }}>
-          {budgets.map(budget =>{
-            const amount = getBudgetExpenses(budget.id).reduce(
-              (total, expense) => total + expense.amount, 0)
-            return (
-              <BudgetCard 
-                key={budget.id}
-                name={budget.name} 
-                amount={amount} 
-                max={budget.max}
-                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
-                onViewExpensesClick={() => setViewExpensesModalBudgetId(budget.id)}
-              />
-            )   
-        })}
-        <UncategorizedBudgetCard 
-        onAddExpenseClick= {openAddExpenseModal}
-        onViewExpensesClick={() => setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)}/>
-        <TotalBudgetCard/>
-        </div>
+    
+    <div className="App">
+
+    
+      <Sidebar></Sidebar>
+
+      {/* Monthly Budgets Container */}
+      <div className="budgets">
+
+      
+        <Container className="my-4" >
+
+          <Stack direction="horizontal" gap="2" className="mb-4">
+            {/*Heading for The Budgets*/}
+            <h1 className="me-auto">Monthly Budgets</h1>
+
+            {/* Buttons in heading */}
+            <Button variant="primary" onClick={()=> setShowAddBudgetModal(true)}>Add Category</Button>
+            <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
+          </Stack> 
+
+          {/* style for the div for budget cards */}
+          <div style={{ 
+              display: "grid", 
+              gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+              gap: "1rem",
+              alignItems: "flex-start",
+          }}>
+            {/* For each budget create a budget card */}
+            {budgets.map(budget =>{
+              const amount = getBudgetExpenses(budget.id).reduce(
+                (total, expense) => total + expense.amount, 0)
+              return (
+                      <BudgetCard 
+                        key={budget.id}
+                        name={budget.name} 
+                        amount={amount} 
+                        max={budget.max}
+                        onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+                        onViewExpensesClick={() => setViewExpensesModalBudgetId(budget.id)}
+                      />
+                      )   
+            })}
+
+            {/* add uncategorized and total budget cards */}
+            <UncategorizedBudgetCard onAddExpenseClick= {openAddExpenseModal} onViewExpensesClick={() => setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)}/>
+            <TotalBudgetCard/>
+          </div>
+        </Container>
+      
+      </div>
 
 
-      </Container>
+      {/* MODALS */}
       <AddBudgetModal 
         show={showAddBudgetModal} 
         handleClose={()=> setShowAddBudgetModal(false)} 
@@ -79,6 +103,10 @@ function App() {
         budgetId={viewExpensesModalBudgetId}
         handleClose={()=> setViewExpensesModalBudgetId()} 
       />
+      
+
+    </div>
+
     </>
   )
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import "../App.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFile, faHouse, faMoneyBillTransfer } from '@fortawesome/free-solid-svg-icons'
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+
+const houseIcon = <FontAwesomeIcon icon={faHouse} />
+const educationIcon = <FontAwesomeIcon icon={faGraduationCap} />
+const currencyIcon = <FontAwesomeIcon icon={faMoneyBillTransfer} />
+const fileIcon = <FontAwesomeIcon icon={faFile} />
+
+export default function Sidebar() {
+  return (
+    <>    
+    <div className="side-nav-container">      
+      <div className="nav-brand">
+        <img src="icons/pocketChangeLogo.svg" alt="nav-logo" class="pcLogo"/>
+        <img src="icons/pocketChangeIcon.svg" alt="nav-icon" class="pcIcon"/>
+      </div>
+      <div className="nav-menu">
+        <ul>
+          <li><a href="#"><i>{houseIcon}</i><p>Home</p></a></li>
+          <li><a href="#"><i>{educationIcon}</i><p>Education</p></a></li>
+          <li><a href="#"><i>{currencyIcon}</i><p>Currency</p></a></li>
+          <li><a href="#"><i>{fileIcon}</i><p>Files</p></a></li>
+        </ul>
+      </div>      
+    </div>
+    </>
+  )
+}
